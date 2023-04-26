35386de9e9bfe065b964087f5cc62b8c0b8bc7e3
KateRiabets <113562687+KateRiabets@users.noreply.github.com>
2023-04-26 17:07:34 +0300

    Add files via upload

---
+++ theme5/lektion1/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    public static function fakeData()
+    {
+        return [
+            ['id' => 1, 'name' => 'Product 1', 'price' => 100],
+            ['id' => 2, 'name' => 'Product 2', 'price' => 200],
+            ['id' => 3, 'name' => 'Product 3', 'price' => 300],
+        ];
+    }
+}
